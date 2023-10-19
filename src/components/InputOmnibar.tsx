dd24ff91050c4861726932ff036a114715f45e6f
Kate Novak <kje@cochleoid.com>
2023-10-18 21:04:08 -0400

    Omnibar Clear Button
    A clear button for the omnibar. You can also press escape to clear the search term.

--- src/components/InputOmnibar.tsx
+++ src/components/InputOmnibar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface InputOmnibarProps {
   onChange: (query: string) => any;
@@ -8,6 +8,7 @@ interface InputOmnibarProps {
 const textAndPaddingClasses = 'text-sm p-2.5';
 
 export default function InputOmnibar(props: InputOmnibarProps) {
+  const queryRef = useRef(null as null | HTMLInputElement);
   const [query, setQuery] = useState('');
 
   function updateQuery(newQuery: string) {
@@ -17,12 +18,28 @@ export default function InputOmnibar(props: InputOmnibarProps) {
 
   function clearQuery() {
     updateQuery('');
+    if (queryRef.current) {
+      queryRef.current.focus();
+    }
   }
+
+  useEffect(() => {
+    function callback(e: KeyboardEvent) {
+      console.log('clearning now');
+      if (e.code === 'Escape') {
+        clearQuery();
+      }
+    }
+    window.addEventListener('keyup', callback);
+    return () => {
+      window.removeEventListener('keyup', callback);
+    };
+  })
 
   // TODO: I need to test that the label here DOES get read by a screen reader. I believe this is how to do it with the hidden class and aria-hidden="false".
   return <div className="sticky top-1">
     <label htmlFor="omnibarInput" className="hidden" aria-hidden="false">Type commands here:</label>
-    <input type="search" id="omnibarInput"
+    <input type="search" id="omnibarInput" ref={queryRef}
       autoFocus={props.autoFocus}
       value={query}
       placeholder="Type help for help."
@@ -32,11 +49,6 @@ export default function InputOmnibar(props: InputOmnibarProps) {
         text-[#223] bg-white/75 border-slate-200
         dark:text-[#EEE] dark:bg-black/60 dark:border-slate-600
       `}
-      onKeyUp={(e) => {
-        if (e.code === 'Escape') {
-          updateQuery('');
-        }
-      }}
       onChange={(e) => {
         updateQuery(e.target.value);
       }}
