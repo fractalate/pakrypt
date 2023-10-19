54ad28955fedf3a5602d977dcbc2144c51fa5a6d
Kate Novak <kje@cochleoid.com>
2023-10-18 20:41:37 -0400

    Omnibar Polishing
    The omnibar is now sticky at the top. It has a clear X and you can press escape to clear it too.

--- src/components/InputOmnibar.tsx
+++ src/components/InputOmnibar.tsx
@@ -1,22 +1,48 @@
+import { useState } from "react";
+
 interface InputOmnibarProps {
   onChange: (query: string) => any;
   autoFocus?: boolean;
 }
 
+const textAndPaddingClasses = 'text-sm p-2.5';
+
 export default function InputOmnibar(props: InputOmnibarProps) {
+  const [query, setQuery] = useState('');
+
+  function updateQuery(newQuery: string) {
+    setQuery(newQuery);
+    props.onChange(newQuery);
+  }
+
+  function clearQuery() {
+    updateQuery('');
+  }
+
   // TODO: I need to test that the label here DOES get read by a screen reader. I believe this is how to do it with the hidden class and aria-hidden="false".
-  return <>
+  return <div className="sticky top-1">
     <label htmlFor="omnibarInput" className="hidden" aria-hidden="false">Type commands here:</label>
-    <input type="text" id="omnibarInput" autoFocus={props.autoFocus}
+    <input type="search" id="omnibarInput"
+      autoFocus={props.autoFocus}
+      value={query}
       placeholder="Type help for help."
-      className="
-        border text-sm rounded-lg block w-full p-2.5
+      className={`
+        sticky top-1
+        border rounded-lg block w-full ${ textAndPaddingClasses }
         text-[#223] bg-white/75 border-slate-200
         dark:text-[#EEE] dark:bg-black/60 dark:border-slate-600
-      "
+      `}
+      onKeyUp={(e) => {
+        if (e.code === 'Escape') {
+          updateQuery('');
+        }
+      }}
       onChange={(e) => {
-        props.onChange(e.target.value);
+        updateQuery(e.target.value);
       }}
     />
-  </>
+    <div className={`absolute top-0 right-0 ${ textAndPaddingClasses }`}>
+      <button tabIndex={-1} className="text-black/50 dark:text-white/50" onClick={clearQuery}>&#x2716;</button>
+    </div>
+  </div>
 }
