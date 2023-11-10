7a787f2ca91e4403264ea2b9e0626bd8f5f291ac
Kate Novak <kje@cochleoid.com>
2023-11-10 08:39:19 -0500

    Cleanup
    Cleaning up trash folder.

--- src/lib/layout.ts
+++
@@ -1,1 +0,0 @@
-export const LayoutStickyControls = "sticky top-1";
--- src/trash/OverlayContainer.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function OverlayContainer(props: PropsWithChildren) {
-  return <div className="overlay-container">
-    {props.children}
-  </div>
-}
